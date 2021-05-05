9da5e1350c32aaa66e87f3c35ff2c05a9d3493ef
Suhas Suresh <suhas.suresh33@gmail.com>
2021-05-05 20:27:09 +0530

    Add files via upload

---
+++ Python_core_assignment/name.py
@@ -0,0 +1,3 @@
+name = raw_input("Enter your first and last name seperated by a space\n")
+name_list = name.split()
+print (name_list[1]+" "+name_list[0])
---
+++ Python_core_assignment/volume.py
@@ -0,0 +1,4 @@
+diameter = 12
+pi = 3.14
+volume  = (4.0/3)*pi*((diameter/2)**3)
+print (volume)
