aa0896791bf8178c4586eaf07f46a8a034f48d32
Suhas Suresh <suhas.suresh33@gmail.com>
2021-05-12 16:51:59 +0530

    Delete Python_core_assignment directory

--- Python_core_assignment/name.py
+++
@@ -1,3 +0,0 @@
-name = raw_input("Enter your first and last name seperated by a space\n")
-name_list = name.split()
-print (name_list[1]+" "+name_list[0])
--- Python_core_assignment/volume.py
+++
@@ -1,4 +0,0 @@
-diameter = 12
-pi = 3.14
-volume  = (4.0/3)*pi*((diameter/2)**3)
-print (volume)
